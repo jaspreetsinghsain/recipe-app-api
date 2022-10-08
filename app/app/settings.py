7c0185a7457be6ae1e90755993a463dc7f080b18
jaspreet singh <sainijaspreet1996@gmail.com>
2022-10-09 00:22:15 +0530

    fixed Syntax Error

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bbgs$m=xvxtrts60-j%jg#h0b5ugjta#9*s9ar9-#7w0xzx#yh'
+SECRET_KEY = 'django-insecure-bbgs$m=xvxtrts60-j%jg#h0b5ugjta#9* \
+s9ar9-#7w0xzx#yh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,22 +81,24 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAtt\
+            ributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.Minimum\
+            LengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.Common\
+            PasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.Numeric\
+            PasswordValidator',
     },
 ]
 
